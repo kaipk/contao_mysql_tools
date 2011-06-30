73d4bfe9422bf89a1b8c41c3ab0a7750e7a65fbc
Leo Unglaub <leo.unglaub@2create.at>
2011-06-30 16:43:26 +0200

    - fix some typos in the german language file

--- system/modules/mysql_tools/languages/de/mysql_tools.php
+++ system/modules/mysql_tools/languages/de/mysql_tools.php
@@ -33,8 +33,8 @@
  * MySQL Tools
  */
 $GLOBALS['TL_LANG']['mysql_tools']['headline'] = 'MySQL Tools';
-$GLOBALS['TL_LANG']['mysql_tools']['description'] = 'Mit den MySQL Tools für Contao können Sie ihre Datenbank warten, oprimieren oder im Notfall auch reparieren. Alle Wartungsbefehle sind MySQL interne Tools und werden über Query\'s angesprochen. Daher benötigen Sie eine funktionierende Datenbankverbindung um den mit MySQLTools arbeiten zu können.';
-$GLOBALS['TL_LANG']['mysql_tools']['warning'] = 'Bitte beachten Sie, dass bei falscher Bedienung dieser Extension unter Umständen irreperabele Schäden an der Datenbank und somit an Ihrer Contao Webseite entstehen können. Wenn Sie sich nicht mit Datenbankwartung und SQL auskennen, sollten sie die Erweiterung nicht einsetzen und zuerst die Dokumentation lesen: <a href="http://dev.mysql.com/doc/refman/5.1/de/table-maintenance-sql.html">MySQL Dokumentation zur Tabellenwartung</a>';
+$GLOBALS['TL_LANG']['mysql_tools']['description'] = 'Mit den MySQL Tools für Contao können Sie ihre Datenbank warten, optimieren oder im Notfall auch reparieren. Alle Wartungsbefehle sind MySQL interne Tools und werden über Queryies angesprochen. Daher benötigen Sie eine funktionierende Datenbankverbindung um den mit MySQLTools arbeiten zu können.';
+$GLOBALS['TL_LANG']['mysql_tools']['warning'] = 'Bitte beachten Sie, dass bei falscher Bedienung dieser Extension unter Umständen irreparable Schäden an der Datenbank und somit an Ihrer Contao Webseite entstehen können. Wenn Sie sich nicht mit Datenbankwartung und SQL auskennen, sollten sie die Erweiterung nicht einsetzen und zuerst die Dokumentation lesen: <a href="http://dev.mysql.com/doc/refman/5.1/de/table-maintenance-sql.html">MySQL Dokumentation zur Tabellenwartung</a>';
 $GLOBALS['TL_LANG']['mysql_tools']['submit'] = 'Los';
 $GLOBALS['TL_LANG']['mysql_tools']['check'] = 'Prüfen (check)';
 $GLOBALS['TL_LANG']['mysql_tools']['analyze'] = 'Analysieren (analyze)';
